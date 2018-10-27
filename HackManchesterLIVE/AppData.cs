0810fdfd967bfd7ff1abc73944d1a09f8ffd1fe3
WeeMick <michaelblr5@gmail.com>
2018-10-27 21:02:03 +0100

    mb do not ignore file

---
+++ HackManchesterLIVE/AppData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackManchesterLIVE
+{
+    class AppData
+    {
+
+        public List<Users> userList;
+        public Dictionary<Users, string> userDict;
+
+        public AppData()
+            {
+                userList = new List<Users>();
+                userDict = new Dictionary<Users, string>();
+            }
+
+        public void addUser(Users user)
+        {
+            userList.Add(user);
+        }
+         
+
+    }
+}
