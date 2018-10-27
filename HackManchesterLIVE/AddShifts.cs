1c73cc4d807e459cd54f806fb079ac6bce070709
elenasecara <43499603+elenasecara@users.noreply.github.com>
2018-10-27 16:46:22 +0100

    es forms created

---
+++ HackManchesterLIVE/AddShifts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddTrips
+{
+    public partial class AddShifts : Form
+    {
+        public AddShifts()
+        {
+            InitializeComponent();
+        }
+
+        private void AddShifts_Load(object sender, EventArgs e)
+        {
+            startTimeTb.Focus();
+        }
+
+        private void startTimeTb_Enter(object sender, EventArgs e)
+        {
+            startTimeTb.Text = "";
+        }
+
+        private void finishTimeTb_Enter(object sender, EventArgs e)
+        {
+            finishTimeTb.Text = "";
+        }
+
+        private void startDateTb_Enter(object sender, EventArgs e)
+        {
+            startDateTb.Text = "";
+        }
+
+        private void finishDateTb_Enter(object sender, EventArgs e)
+        {
+            finishDateTb.Text = "";
+        }
+    }
+}
